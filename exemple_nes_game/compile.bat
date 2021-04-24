cbc406f3f85c59cdc48b78e0279e54da63982fb3
abdelghaniBzn <bouziane.abdelghani@gmail.com>
2021-04-24 21:16:42 +0200

    -- add exemple of nes game development.

---
+++ exemple_nes_game/compile.bat
@@ -0,0 +1,3 @@
+assembler\WinASM65.exe -m game.json
+assembler\WinASM65.exe -c rom_gen.json
+pause
---
+++ exemple_nes_game/engine/background.asm
@@ -0,0 +1,82 @@
+  .org $C000 
+RESET:
+  SEI          ; disable IRQs
+  CLD          ; disable decimal mode
+  LDX #$40
+  STX $4017    ; disable APU frame IRQ
+  LDX #$FF
+  TXS          ; Set up stack
+  INX          ; now X = 0
+  STX $2000    ; disable NMI
+  STX $2001    ; disable rendering
+  STX $4010    ; disable DMC IRQs
+
+vblankwaitf:       ; First wait for vblank to make sure PPU is ready
+  BIT $2002
+  BPL vblankwaitf
+
+clrmem:
+  LDA #$00
+  STA $0000, x
+  STA $0100, x
+  STA $0200, x
+  STA $0400, x
+  STA $0500, x
+  STA $0600, x
+  STA $0700, x
+  LDA #$FE
+  STA $0300, x
+  INX
+  BNE clrmem
+   
+vblankwaits:      ; Second wait for vblank, PPU is ready after this
+  BIT $2002
+  BPL vblankwaits
+
+
+  ; ************** NEW CODE ****************
+LoadPalettes:
+  LDA $2002    ; read PPU status to reset the high/low latch
+  LDA #$3F
+  STA $2006    ; write the high byte of $3F00 address
+  LDA #$00
+  STA $2006    ; write the low byte of $3F00 address
+  LDX #$00
+LoadPalettesLoop:
+  LDA palette, x        ;load palette byte
+  STA $2007             ;write to PPU
+  INX                   ;set index to next byte
+  CPX #$20            
+  BNE LoadPalettesLoop  ;if x = $20, 32 bytes copied, all done
+
+
+
+  LDA #$80
+  STA $0200        ; put sprite 0 in center ($80) of screen vert
+  STA $0203        ; put sprite 0 in center ($80) of screen horiz
+  LDA #$00
+  STA $0201        ; tile number = 0
+  STA $0202        ; color = 0, no flipping
+
+  LDA #%10000000   ; enable NMI, sprites from Pattern Table 0
+  STA $2000
+
+  LDA #%00010000   ; enable sprites
+  STA $2001
+
+Forever:
+  JMP Forever     ;jump back to Forever, infinite loop
+  
+ 
+
+NMI:
+  LDA #$00
+  STA $2003  ; set the low byte (00) of the RAM address
+  LDA #$02
+  STA $4014  ; set the high byte (02) of the RAM address, start the transfer
+  
+  RTI        ; return from interrupt
+
+palette:
+  .byte $0F,$31,$32,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F
+  .byte $0F,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C
