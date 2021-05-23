d613df86d5bfae6df2996bde5faeab73bfd56fa0
abdelghaniBzn <bouziane.abdelghani@gmail.com>
2021-05-23 21:33:57 +0200

    -- update build.

--- exemple_nes_game/compile.bat
+++ exemple_nes_game/compile.bat
@@ -1,4 +1,7 @@
-assembler\WinASM65.exe -m game.json
-assembler\WinASM65.exe -c rom_gen.json
+del engine\*.o
+del engine\*.o_Symbol.txt
+del generated_Nes_Rom\*.nes
+WinASM65 -m game.json
+WinASM65 -c rom_gen.json
 generated_Nes_Rom\Mesen\Mesen generated_Nes_Rom\game.nes 
 pause
